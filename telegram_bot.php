21d91cc90cfd484a4e3bdd8bebb2dfb1bdc55556
Luan Michel <luanmichel96@gmail.com>
2020-05-08 03:48:45 -0300

    Adição de mais opçṍes no menu e mais cidades

--- telegram_bot.php
+++ telegram_bot.php
@@ -17,23 +17,40 @@
                 //envia a mensagem ao usuário
                 sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => 'Olá, '. $message['from']['first_name'].
                     '! Eu sou um bot que informa a previsão do tempo', 'reply_markup' => array(
-                    'keyboard' => array(array('Previsao')),
+                    'keyboard' => array(array('Previsão')),
                     'one_time_keyboard' => true)));
             } else if ($text === "Previsao") {
-                sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => 'Escolha de qual 
-                    cidade você deseja receber a previsão.', 'reply_markup' => array(
-                    'keyboard' => array(array('Ponta Grossa', 'Castro'), array('Carambeí')),
+                sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => 'Escolha de qual cidade você deseja receber a previsão.', 
+                    'reply_markup' => array(
+                    'keyboard' => array(array('Ponta Grossa', 'Castro', 'Carambeí', 'Jaguariaíva'), 
+                                        array('Tibagi', 'Telêmaco Borba', 'Reserva', 'Irati'),
+                                        array('Ipiranga', 'Prudentópolis', 'Ivaí', 'Todas')),
                     'one_time_keyboard' => true)));
 
             } else if ($text === "Ponta Grossa") {
                 sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getCity('6401')));
-
             } else if ($text === "Castro") {
                 sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getCity('3357')));
-
             } else if ($text === "Carambeí") {
                 sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getCity('6711')));
-
+            } else if ($text === "Jaguariaíva") {
+                sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getCity('6589')));
+            } else if ($text === "Tibagi") {
+                sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getCity('6249')));
+            } else if ($text === "Telêmaco Borba") {
+                sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getCity('6224')));
+            } else if ($text === "Reserva") {
+                sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getCity('6472')));
+            } else if ($text === "Irati") {
+                sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getCity('6994')));
+            } else if ($text === "Ipiranga") {
+                sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getCity('6573')));
+            } else if ($text === "Prudentópolis") {
+                sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getCity('5615')));
+            } else if ($text === "Ivaí") {
+                sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getCity('6584')));
+            } else {
+                sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => 'Desculpe, não entendi o que disse.'));
             }
 
         } else {
